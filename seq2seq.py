86d3be0e2f62b910b5e64cce08b90956efdced8f
Boris Polonsky <BorisPolonsky@users.noreply.github.com>
2020-03-25 17:20:22 +0800

    Update Test Code for GPU

--- seq2seq.py
+++ seq2seq.py
@@ -138,14 +138,17 @@ class Seq2SeqAttn(torch.nn.Module):
 
 
 if __name__ == "__main__":
-    enc_inputs = torch.tensor([[1, 2, 3, 4], [1, 2, 3, 0]])
+    device = None
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    enc_inputs = torch.tensor([[1, 2, 3, 4], [1, 2, 3, 0]], device=device)
     sequence_length = torch.tensor([4, 3])
     enc_hidden_dim = 100
     dec_hidden_dim = 200
-    dec_cur_input = torch.tensor([1, 2])
+    dec_cur_input = torch.tensor([1, 2], device=device)
     nn = Seq2SeqAttn(vocab_size_f=100, embedding_dim_f=64, vocab_size_e=100, embedding_dim_e=64,
                      enc_hidden_dim=enc_hidden_dim,
-                     dec_hidden_dim=dec_hidden_dim)
+                     dec_hidden_dim=dec_hidden_dim).to(device)
     dec_state = torch.zeros([dec_cur_input.size(0), dec_hidden_dim], device=dec_cur_input.device)
     for i in range(5):
         out, dec_state = nn.decode_one_step_forward(enc_inputs, sequence_length, dec_cur_input, dec_state)
